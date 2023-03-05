49f19d485d7acfcfc665054df4ca41da9c09ee1b
Julio Flowers <julio_alfonso2000@hotmail.com>
2023-03-04 20:47:55 -0600

    nuevorep

---
+++ factorial.py
@@ -0,0 +1,8 @@
+def fact (x): # definimos la funcion fact
+ if x ==0: # caso base
+     return 1
+ else :
+     return x* fact (x -1) # para los demas casos
+
+x= int ( input (" Valor a calcular factorial :") ) #dar valor
+print ( fact (x)) # imprime el resultado
