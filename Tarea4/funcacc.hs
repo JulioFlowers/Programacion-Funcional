8fa4cfb07f898b2b1b1d3a51174da5087d741d7e
jflws <julio_perez@ciencias.unam.mx>
2023-06-11 21:42:42 -0600

    finito

--- Tarea4/funcacc.hs
+++ Tarea4/funcacc.hs
@@ -6,3 +6,112 @@ Similar al caso del satélite y el oscilador forzado-amortiguado, implementala f
 de aceleración que es repelido por una carga estática de la misma magnitud  y  signo.
 Evalúa  el  sistema  dinámico  y  da  algunos  valores  del resultado.
 -}
+
+
+-- Definción de la estructura de Vector.
+data Vec = Vec { xComp :: Double,
+                 yComp :: Double,
+                 zComp :: Double} deriving Show
+
+type Temp = Double
+type Desp = Vec
+type Vel = Vec
+type Est = ( Temp , Desp , Vel )
+
+type FunAcc =  Est -> Vec
+
+--SUMA VEC: Se suma componente a componente de 2 estructuras de datos Vector.
+(^+^) :: Vec -> Vec -> Vec
+a ^+^ b = Vec (xComp a + xComp b) (yComp a + yComp b) (zComp a + zComp b)
+
+--RESTA VEC: Se resta componente a compoyente de 2 estructuras de datos Vector.
+(^-^) :: Vec -> Vec -> Vec
+a ^-^ b = Vec (xComp a - xComp b) (yComp a - yComp b) (zComp a - zComp b)
+
+{- PRODUCTO ESCALAR IZQ: Se multiplica el escalar por cada componente del vector, por la izquierda -}
+(^*) :: Double -> Vec -> Vec
+a ^* b = Vec (a * xComp b) (a * yComp b) (a *zComp b)
+
+{-PRODUCTO ESCALAR DER: Se multiplica el escalar por cada componente del vector, por la derecha-}
+(*^) :: Vec -> Double -> Vec
+a *^ b = Vec (xComp a * b) (yComp a * b) (zComp a *b)
+
+{-PRODUCTO VEC: Se multiplica componente a componente de 2 estructuras de datos Vector y se suman-}
+(^.^) :: Vec -> Vec -> Double
+a ^.^ b = (xComp a * xComp b) + (yComp a * yComp b) + (zComp a * zComp b)
+
+{-PRODUCTO CRUZ: Se define como la regla nemotecnica-}
+pX :: Vec -> Vec -> Vec
+pX a b = Vec (yComp a * zComp b - zComp a * yComp b)
+             (zComp a * xComp b - xComp a * zComp b) 
+             (xComp a * yComp b - yComp a * xComp b)
+
+{-NORMA: Define como la raiz cuadrada del producto punto -}
+mag :: Vec -> Double
+mag a = sqrt(a ^.^ a)
+
+{-Aqui no entendi que especificaba el profe asi que hice 2 operaciones. La división por un escalar 
+que realmente es una multiplicación escalar por la izq usando el inverso del numero proporcionado-}
+(^/) :: Double -> Vec -> Vec
+a ^/ b = Vec (ai * xComp b) (ai * yComp b) (ai *zComp b)
+         where ai = (1/)a
+
+--La segunda es la normalización de un vector 
+unit  :: Vec -> Vec
+unit a = (mag a) ^/ a
+
+{-Tenemos la sig. expresión
+
+                     a = F_e / m, 
+
+ como solo tenemos 2 pariculas con carga interactuando
+ tenemos de la ley de Coulomb:
+
+                  a = (K/m) (2q / |r²|) r
+
+para mayor facilidad supongamos que estamos en el vacio
+
+                K  = 1 / 4*pi*e_o 
+-}
+
+--Definicon de la función
+accRep :: FunAcc 
+accRep (t, r, v) = (k * (2*q/mag r ^2)) ^* u
+      where
+           k= 1 / 4*pi*ep_0*m
+           ep_0 = 8.8549E-12 --[C²N⁻¹m⁻²]
+           q = -1.6E-19 --[C] carga electrón
+           m = 9.1E-31 --[kg] masa electrón
+           u = unit r -- vector unitario r
+
+
+
+-- Aplicacion del metodo de euler
+pasoEuler :: FunAcc -> Double -> Est -> Est
+pasoEuler a dt (t , r , v) = (t',r' ,v')
+    where
+         t' = t + dt
+         r' = r ^+^ v *^ dt
+         v' = v ^+^ a (t ,r ,v) *^ dt
+
+-- Esta funcion itera sobre el la funcion pasoEuler
+euler :: FunAcc -> Double -> Est -> [ Est ]
+euler a dt = iterate ( pasoEuler a dt )
+
+{-Condiciones Iniciales
+
+  posición: Reulizamos las mismas del satelite,
+
+  velocidad: Partimos del reposo.
+-}
+rr :: Vec
+rr = Vec 1.0 (-2.1) 0.5
+
+vv :: Vec
+vv = Vec 0 0 0
+
+
+calculo = do
+   --Le pedimos a la iteracion 10 estados 
+   let estados = take 10 $ euler accRep 0.01 (1.0 , rr , vv )
+   putStrLn (show estados) 
--- Tarea4/trapecios.hs
+++ Tarea4/trapecios.hs
@@ -6,3 +6,191 @@ Ejercicio 2: Usa el método del trapecio y el punto medio para resolver las inte
 Trata de comparar con resultados analíticos o de otros métodos (es decir, checa con las
 tablas) ¿qué tanto es el error? ¿qué puedes hacer parareducirlo?
 -}
+
+--Defincion de funciones 
+type FuncR = Double -> Double
+
+funca :: FuncR
+funca x = x^2
+
+funcb :: FuncR
+funcb x = x * exp x
+
+funcc :: FuncR
+funcc x = x^2 * cos x
+
+{-calcula el error relativo de la integral numerica
+con respecto a la integral analitica -}
+errel :: Double -> Double -> Double 
+errel ia ni = abs (ia - ni) / ia * 100
+
+--REGLA DEL PUNTO MEDIO
+pMint :: FuncR -> Double -> Double -> Double -> Double
+pMint f a b dt = 
+    sum [f t * dt | t <-[a + dt/2, a + 3*dt/2 .. b - dt/2]]
+
+--REGLA DEL TRAPECIO 
+tintg :: FuncR -> Double -> Double -> Double -> Double -> Double -> Double
+tintg f x n lim0 newlim0 lim1
+      |newlim0 >= lim1 = (x * delta)
+      |x == 0 = tintg f (x + (f lim0 + f lim1)/2) n lim0 (newlim0 + delta) lim1
+      |otherwise = tintg f (x + f newlim0) n lim0 (newlim0 + delta) lim1
+      where delta = ((lim1-lim0)/n)
+--inciso a)
+-- Integracion analitica Analitica = 1/3
+
+incisoa = do
+
+    --integracion por punto medio con un paso de 0.5
+    let ipM05 = pMint funca 0 1 0.5
+    --calculo del error
+    let ipM05er = errel (1/3) ipM05
+    putStrLn ("Inregracion x² de 0 al 1 con dt = 0.5 y metodo del punto medio: " ++ show(ipM05))
+    putStrLn ("Error relativo comparado con el valor de la integración analitica (1/3): " ++ show(ipM05er) ++ " %")
+
+
+    --integracion por punto medio con un paso de 0.1
+    let ipM01 = pMint funca 0 1 0.1
+    --calculo del error
+    let ipM01er = errel (1/3) ipM01
+    putStrLn ("Inregracion x² de 0 al 1 con dt = 0.1 y metodo del punto medio: " ++ show(ipM01))
+    putStrLn ("Error relativo comparado con el valor de la integración analitica (1/3): " ++ show(ipM01er) ++ " %")
+
+     --integracion por punto medio con un paso de 0.01
+    let ipM001 = pMint funca 0 1 0.01
+    --calculo del error
+    let ipM001er = errel (1/3) ipM001
+    putStrLn ("Inregracion x² de 0 al 1 con dt = 0.01 y metodo del punto medio: " ++ show(ipM001))
+    putStrLn ("Error relativo comparado con el valor de la integración analitica (1/3): " ++ show(ipM001er) ++ " %")
+
+    putStrLn ""
+    putStrLn ""
+
+    --integracion por trapecio con 10 particiones
+    let tintgM05 = tintg funca 0 10 0 0 1
+    --calculo del error
+    let tintgM05er = errel (1/3) tintgM05
+    putStrLn ("Inregracion x² de 0 al 1 haciendo 10 particiones con el metodo trapecio: " ++ show(tintgM05))
+    putStrLn ("Error relativo comparado con el valor de la integración analitica (1/3): " ++ show(tintgM05er) ++ " %")
+
+    --integracion por trapecio con 10 particiones
+    let tintgM05 = tintg funca 0 100 0 0 1
+    --calculo del error
+    let tintgM05er = errel (1/3) tintgM05
+    putStrLn ("Inregracion x² de 0 al 1 haciendo 100 particiones con el metodo trapecio: " ++ show(tintgM05))
+    putStrLn ("Error relativo comparado con el valor de la integración analitica (1/3): " ++ show(tintgM05er) ++ " %")
+
+    --integracion por trapecio con 10 particiones
+    let tintgM05 = tintg funca 0 1000 0 0 1
+    --calculo del error
+    let tintgM05er = errel (1/3) tintgM05
+    putStrLn ("Inregracion x² de 0 al 1 haciendo 1000 particiones con el metodo trapecio: " ++ show(tintgM05))
+    putStrLn ("Error relativo comparado con el valor de la integración analitica (1/3): " ++ show(tintgM05er) ++ " %")
+
+
+
+
+--inciso b)
+-- Integracion analitica Analitica = 1
+
+incisob = do
+
+    --integracion por punto medio con un paso de 0.5
+    let ipM01 = pMint funcb 0 1 0.5
+    --calculo del error
+    let ipM01er = errel (1) ipM01
+    putStrLn ("Inregracion xexp(x) de 0 al 1 con dt = 0.5 y metodo del punto medio: " ++ show(ipM01))
+    putStrLn ("Error relativo comparado con el valor de la integración analitica (1): " ++ show(ipM01er) ++ " %")
+
+
+    --integracion por punto medio con un paso de 0.1
+    let ipM01 = pMint funcb 0 1 0.1
+    --calculo del error
+    let ipM01er = errel (1) ipM01
+    putStrLn ("Inregracion xexp(x) de 0 al 1 con dt = 0.1 y metodo del punto medio: " ++ show(ipM01))
+    putStrLn ("Error relativo comparado con el valor de la integración analitica (1): " ++ show(ipM01er) ++ " %")
+
+     --integracion por punto medio con un paso de 0.01
+    let ipM001 = pMint funcb 0 1 0.01
+    --calculo del error
+    let ipM001er = errel (1) ipM001
+    putStrLn ("Inregracion xexp(x) de 0 al 1 con dt = 0.01 y metodo del punto medio: " ++ show(ipM001))
+    putStrLn ("Error relativo comparado con el valor de la integración analitica (1): " ++ show(ipM001er) ++ " %")
+
+    putStrLn ""
+    putStrLn ""
+
+     --integracion por trapecio con 10 particiones
+    let tintgM05 = tintg funcb 0 10 0 0 1
+    --calculo del error
+    let tintgM05er = errel 1 tintgM05
+    putStrLn ("Inregracion xexp(x) de 0 al 1 haciendo 10 particiones con el metodo trapecio: " ++ show(tintgM05))
+    putStrLn ("Error relativo comparado con el valor de la integración analitica (1): " ++ show(tintgM05er) ++ " %")
+
+    --integracion por trapecio con 10 particiones
+    let tintgM05 = tintg funcb 0 100 0 0 1
+    --calculo del error
+    let tintgM05er = errel 1 tintgM05
+    putStrLn ("Inregracion xexp(x) de 0 al 1 haciendo 100 particiones con el metodo trapecio: " ++ show(tintgM05))
+    putStrLn ("Error relativo comparado con el valor de la integración analitica (1/3): " ++ show(tintgM05er) ++ " %")
+
+    --integracion por trapecio con 10 particiones
+    let tintgM05 = tintg funcb 0 1000 0 0 1
+    --calculo del error
+    let tintgM05er = errel 1 tintgM05
+    putStrLn ("Inregracion xexp(x) de 0 al 1 haciendo 1000 particiones con el metodo trapecio: " ++ show(tintgM05))
+    putStrLn ("Error relativo comparado con el valor de la integración analitica (1): " ++ show(tintgM05er) ++ " %")
+
+
+
+
+--inciso c)
+-- Integracion analitica Analitica = pi²/4 - 2 aprox(0.46740)
+
+incisoc = do
+
+    --integracion por punto medio con un paso de 0.5
+    let ipM01 = pMint funcc 0 (pi/2) 0.5
+    --calculo del error
+    let ipM01er = errel (pi**2 / 4 - 2) ipM01
+    putStrLn ("Inregracion x²cos(x) de 0 al pi/2 con dt = 0.5 y metodo del punto medio: " ++ show(ipM01))
+    putStrLn ("Error relativo comparado con el valor de la integración analitica (pi²/4 - 2 aprox(0.46740)): " ++ show(ipM01er) ++ " %")
+
+
+    --integracion por punto medio con un paso de 0.1
+    let ipM01 = pMint funcc 0 (pi/2) 0.1
+    --calculo del error
+    let ipM01er = errel (pi**2 / 4 - 2) ipM01
+    putStrLn ("Inregracion  x²cos(x) de 0 al pi/2 con dt = 0.1 y metodo del punto medio: " ++ show(ipM01))
+    putStrLn ("Error relativo comparado con el valor de la integración analitica (pi²/4 - 2 aprox(0.46740)): " ++ show(ipM01er) ++ " %")
+
+     --integracion por punto medio con un paso de 0.01
+    let ipM001 = pMint funcc 0 (pi/2) 0.01
+    --calculo del error
+    let ipM001er = errel (pi**2 / 4 - 2) ipM001
+    putStrLn ("Inregracion  x²cos(x) de 0 al pi/2 con dt = 0.01 y metodo del punto medio: " ++ show(ipM001))
+    putStrLn ("Error relativo comparado con el valor de la integración analitica (pi²/4 - 2 aprox(0.46740)): " ++ show(ipM001er) ++ " %")
+
+    putStrLn ""
+    putStrLn ""
+
+     --integracion por trapecio con 10 particiones
+    let tintgM05 = tintg funcc 0 10 0 0 (pi/2)
+    --calculo del error
+    let tintgM05er = errel (pi**2 / 4 - 2) tintgM05
+    putStrLn ("Inregracion x²cos(x) de 0 al (pi/2) haciendo 10 particiones con el metodo trapecio: " ++ show(tintgM05))
+    putStrLn ("Error relativo comparado con el valor de la integración analitica (pi²/4 - 2 aprox(0.46740)): " ++ show(tintgM05er) ++ " %")
+
+    --integracion por trapecio con 10 particiones
+    let tintgM05 = tintg funcc 0 100 0 0 (pi/2)
+    --calculo del error
+    let tintgM05er = errel (pi**2 / 4 - 2) tintgM05
+    putStrLn ("Inregracion x²cos(x) de 0 al (pi/2) haciendo 100 particiones con el metodo trapecio: " ++ show(tintgM05))
+    putStrLn ("Error relativo comparado con el valor de la integración analitica (pi²/4 - 2 aprox(0.46740)): " ++ show(tintgM05er) ++ " %")
+
+    --integracion por trapecio con 10 particiones
+    let tintgM05 = tintg funcc 0 1000 0 0 (pi/2)
+    --calculo del error
+    let tintgM05er = errel (pi**2 / 4 - 2) tintgM05
+    putStrLn ("Inregracion x²cos(x) de 0 al (pi/2) haciendo 1000 particiones con el metodo trapecio: " ++ show(tintgM05))
+    putStrLn ("Error relativo comparado con el valor de la integración analitica (pi²/4 - 2 aprox(0.46740)): " ++ show(tintgM05er) ++ " %")
